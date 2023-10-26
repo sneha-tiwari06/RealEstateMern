a1e6482a2719c077e778857930936791f3aaeedb
Sneha Tiwari <snehatiwari0620@gmail.com>
2023-10-26 17:56:28 +0530

    Created controller and router

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -2,8 +2,15 @@ import asyncHandler from "express-async-handler";
 import { prisma } from "../config/prismaConfig.js";
 
 export const createUser = asyncHandler(async (req, res) => {
-    console.log("Creating a user");
+  console.log("Creating a user");
 
-    let { email } = req.body;
-    console.log(email)
+  let { email } = req.body;
+  const userExists = await prisma.user.findUnique({ where: { email: email } });
+  if (!userExists) {
+    const user = await prisma.user.create({ data: req.body });
+    res.send({
+      message: "User Registered sucessfully",
+      user: user,
+    });
+  } else res.status(201).send({ message: "user already registered" });
 });
